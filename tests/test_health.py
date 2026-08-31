29bb0f27e59cca38f3edabb890d71a84125c9c59
kevin <physicsKev@outlook.com>
2026-08-30 21:49:09 -0400

    intentionally added error in 'test_health.py' to break the CI

--- tests/test_health.py
+++ tests/test_health.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 
 def test_health() -> None:
     response = client.get("/health")
-    assert response.status_code == 200
+    assert response.status_code == 500
     assert response.json() == {"status": "ok"}
 
 
